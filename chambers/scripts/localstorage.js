2e9cb66823ff5cdf709b940e9c1755798f620818
https://github.com/aboki-lab2/wdd230.git <parungwa@byui.edu>
2022-11-27 14:52:09 +0100

    delight1

--- chambers/scripts/localstorage.js
+++ chambers/scripts/localstorage.js
@@ -1,18 +1,20 @@
-const visit = document.querySelector('#visit');
+// initialize display elements
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
 
-window.addEventListener("load", () => {
-    let oldDate = localStorage.getItem("lastDate"); 
-    let currentDate = Date.now();
-    if ( oldDate === null || oldDate === ""){
-        oldDate = currentDate;
-    }
-    
-    // let oldDate = 1467442058802;
-    // let convertedDate = new Date(oldDate);
-    let days = Math.trunc((currentDate - oldDate) / 86400000);
-  
-    
-    localStorage.setItem("lastDate", currentDate);
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
-    visit.innerHTML = days;
-});
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// show todays date.
+todayDisplay.textContent = Date.now();
--- chambers/scripts/mydate.js
+++ chambers/scripts/mydate.js
@@ -1,12 +1,22 @@
-// -------------date-----------
-const date=new Date()
-const formattedDate=new Intl.DateTimeFormat("en-UK",{datestyle: "full"}).format(date);
-document.querySelector("#formatted-date").innerHTML=formattedDate;
+
+// -------------todays date - HEADER 08/11/2022-----------
+// select the elements to manipulate (output to)
+const datefieldUK = document.querySelector(".todaysdate"); // for european/family history format with day first. 
+
+// derive the current date using a date object
+const now = new Date();
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`; 
+
 
 // --------year-----------------
-var year=date.getFullYear()
-document.querySelector("#year").innerHTML=year;
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
 
 // ------------last modify------------------
 let d= document.lastModified;
-document.querySelector("#last-modified").innerHTML=d;
+document.querySelector("#lastUpdate").innerHTML=d;
